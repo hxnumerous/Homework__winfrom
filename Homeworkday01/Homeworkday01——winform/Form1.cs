2dae54521fd6c6cf3d6dfd1f5b4d90a5b6843734
xin <2317185076@qq.com>
2026-08-24 19:00:05 +0800

    第一次winform作业

---
+++ Homeworkday01/Homeworkday01——winform/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Homeworkday01__winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
