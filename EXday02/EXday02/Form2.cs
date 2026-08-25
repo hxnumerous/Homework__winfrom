45d0183e79f33bffbc8f34dec7c1593d3e92a479
xin <2317185076@qq.com>
2026-08-25 20:16:16 +0800

    winform第二天练习

---
+++ EXday02/EXday02/Form2.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EXday02
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            initopt();
+        }
+
+
+        private void initopt()
+        {
+            button1.Click += Button1_Click;
+            button2.Click += Button2_Click;
+            button3.Click += Button3_Click;
+        }
+        public void Button1_Click(object sender, EventArgs e)
+        {
+
+            //将button值类型由Object转为button
+            Button btn1 = (Button)sender;
+
+            //添加事件
+            if (btn1.Text == "小猫")
+            {
+                button2.BackColor = Color.DarkGray;
+                button2.ForeColor = Color.Black;
+
+                button3.BackColor = Color.DarkGray;
+                button3.ForeColor = Color.Black;
+
+                // 通过代码给 图片控件 设置 图片资源Image.FromFile(图片路径)
+                pictureBox1.Image = Image.FromFile(@"./images/cat.jpg");
+
+                // 将当前这个按钮的高亮添加
+                Button btn = (Button)sender;
+                btn.BackColor = Color.Cyan;
+                btn.ForeColor = Color.White;
+            }
+
+        }
+
+        public void Button2_Click(object sender, EventArgs e)
+        {
+            //将button值类型由Object转为button
+            Button btn2 = (Button)sender;
+
+            //添加事件
+            if (btn2.Text == "小鸡")
+            {
+
+                button1.BackColor = Color.DarkGray;
+                button1.ForeColor = Color.Black;
+
+                button3.BackColor = Color.DarkGray;
+                button3.ForeColor = Color.Black;
+
+                // 通过代码给 图片控件 设置 图片资源Image.FromFile(图片路径)
+                pictureBox1.Image = Image.FromFile(@"./images/bird.jpg");
+
+                // 将当前这个按钮的高亮添加
+                Button btn = (Button)sender;
+                btn.BackColor = Color.Cyan;
+                btn.ForeColor = Color.White;
+            }
+
+
+        }
+
+        public void Button3_Click(object sender, EventArgs e)
+        {
+            //将button值类型由Object转为button
+            Button btn3 = (Button)sender;
+
+            //添加事件
+            if (btn3.Text == "老鹰")
+            {
+                button1.BackColor = Color.DarkGray;
+                button1.ForeColor = Color.Black;
+
+                button2.BackColor = Color.DarkGray;
+                button2.ForeColor = Color.Black;
+
+                // 通过代码给 图片控件 设置 图片资源Image.FromFile(图片路径)
+                pictureBox1.Image = Image.FromFile(@"./images/eagle.jpg");
+
+                // 将当前这个按钮的高亮添加
+                Button btn = (Button)sender;
+                btn.BackColor = Color.Cyan;
+                btn.ForeColor = Color.White;
+            }
+
+
+
+        }
+
+    }
+}
---
+++ EXday02/EXday02/Form3.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace EXday02
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(799, 450);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(198, 405);
+            button1.Name = "button1";
+            button1.Size = new Size(137, 43);
+            button1.TabIndex = 4;
+            button1.Text = "小猫";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(341, 405);
+            button2.Name = "button2";
+            button2.Size = new Size(137, 43);
+            button2.TabIndex = 3;
+            button2.Text = "小鸡";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(484, 405);
+            button3.Name = "button3";
+            button3.Size = new Size(137, 43);
+            button3.TabIndex = 2;
+            button3.Text = "老鹰";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.cat;
+            pictureBox1.Location = new Point(141, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(527, 397);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Form3";
+            Text = "Form3";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox1;
+    }
+}
